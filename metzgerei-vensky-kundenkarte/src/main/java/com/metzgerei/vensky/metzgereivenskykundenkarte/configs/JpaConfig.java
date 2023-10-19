ba08ebaf7b300adcc6dfeadfe6117511d32105be
Marpelz <marpelz@hotmail.com>
2023-10-19 10:03:22 +0200

    added Methods for DB Connection, added QR-Code generator, some test of other QR-Code Scanner, Card Generator now works

---
+++ metzgerei-vensky-kundenkarte/src/main/java/com/metzgerei/vensky/metzgereivenskykundenkarte/configs/JpaConfig.java
@@ -0,0 +1,2 @@
+package com.metzgerei.vensky.metzgereivenskykundenkarte.configs;public class JpaConfig {
+}
--- metzgerei-vensky-kundenkarte/src/main/java/com/metzgerei/vensky/metzgereivenskykundenkarte/services/ValidationService.java
+++
@@ -1,2 +0,0 @@
-package com.metzgerei.vensky.metzgereivenskykundenkarte.services;public class ValidationService {
-}
